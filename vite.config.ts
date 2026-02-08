d2723cf886e454d977967cec19b3d21e66506b8b
Y0r <panchyk.yuriu@gmail.com>
2026-02-07 22:58:55 -0500

    chore(vite): add react plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
+
 import path from "path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "src",
   envDir: "..",
+  plugins: [react()],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
