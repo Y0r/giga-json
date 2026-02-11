f3fb37ba3fc12cefe59a8e1e9beab7799f270d8f
Y0r <panchyk.yuriu@gmail.com>
2026-02-10 21:10:48 -0500

    fix(StableLayout): Update layout to support default properties

--- src/containers/Layout/StableLayout.tsx
+++ src/containers/Layout/StableLayout.tsx
@@ -1,28 +1,35 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, BoxProps } from "@chakra-ui/react";
+import classNames from "classnames";
 
-interface StableLayoutProps {
+type StableLayoutProps = {
+  className?: string;
   children: React.ReactNode;
-}
+} & Omit<BoxProps, "className" | "children">;
 
 /**
  * Default layout for the application which provides a stable and responsive layout structure.
  *
  * The key feature of this layout is its ability to maintain a consistent layout across different screen sizes and orientations.
  *
- * @param children
+ * @param {string} className - Additional CSS class names to apply to the layout container.
+ * @param {React.ReactNode} children - The content to be rendered within the layout.
+ * @param {array} props - Additional props to be passed to the Box component.
  * @constructor
  */
 export const StableLayout: React.FC<StableLayoutProps> = ({
+  className,
   children,
+  ...props
 }: StableLayoutProps) => {
   return (
     <Box
-      className={"cn-stable-layout"}
+      className={classNames("cn-stable-layout", className)}
       w={"100%"}
       h={"100vh"}
       position={"relative"}
       overflow={"hidden"}
+      {...props}
     >
       {children}
     </Box>
