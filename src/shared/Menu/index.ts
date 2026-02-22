ba764c72a3a474add8a220dde30ea2b4d908819b
Y0r <panchyk.yuriu@gmail.com>
2026-02-22 18:11:56 -0500

    fear(Menu): Update barrel file

--- src/shared/Menu/index.ts
+++ src/shared/Menu/index.ts
@@ -1,17 +1,21 @@
+export * from "./menu.types";
+
 /**
  * Component for creating menus with nested items and flexible layout.
  */
 export { MenuRoot } from "./MenuRoot";
+
 /**
  * Individual menu item logic and rendering.
  */
 export { MenuItem } from "./MenuItem";
+
 /**
  * Trigger element for a menu (button or menu item).
  */
 export { MenuTrigger } from "./MenuTrigger";
+
 /**
  * Content container for menu items, supporting recursion for submenus.
  */
 export { MenuContent } from "./MenuContent";
-export * from "./menu.types";
