d46d6fbddb6b9b46b04ce5552fe36e4ca970bc93
Y0r <panchyk.yuriu@gmail.com>
2026-02-22 21:19:18 -0500

    feat(IDE): Add base structure for ide component

--- src/feature/ide/IDE.tsx
+++ src/feature/ide/IDE.tsx
@@ -1,10 +1,41 @@
-import React from "react";
-import { Editor } from "./Editor/Editor";
+import React, { Fragment } from "react";
+
+import { StableLayout } from "@/shared/Layout/StableLayout";
+import { Layout } from "@/shared/Layout/Layout";
+
+import { Editor, EditorTabs } from "@/feature/ide/components";
+import { MainMenu } from "@/feature/ide/components/Menu/MainMenu";
+import { Logo } from "@/shared/Identica/Logo";
+
+import "@/feature/ide/styles/ide.scss";
+import { Flex, Text } from "@chakra-ui/react";
 
 interface IDEProps {}
 
-export const IDE = (Props: IDEProps) => {
-  return <Editor />;
+export const IDE = (props: IDEProps) => {
+  return (
+    <StableLayout className={"c-ide"}>
+      <Layout className={"c-ide-heading"}>
+        <Flex alignItems={"center"} alignContent={"center"} gap={3} pb={1}>
+          <Logo />
+          <MainMenu />
+        </Flex>
+      </Layout>
+
+      <StableLayout className={"c-ide__content"}>
+        {/* @todo add wrapper to allow orientation change. */}
+        <EditorTabs />
+        <Editor />
+
+        {/* @todo render left sidebar */}
+      </StableLayout>
+
+      <Layout className={"c-ide-footer"}>
+        {/* @todo display file path in footer. */}
+        {/* @todo render goto-line widget */}
+      </Layout>
+    </StableLayout>
+  );
 };
 
 export default IDE;
