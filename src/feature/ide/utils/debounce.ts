6eb37f2908fe2db9c188151f5845d21aee544964
Y0r <panchyk.yuriu@gmail.com>
2026-02-27 20:26:15 -0500

    feat(utils): Add helper to debounce callback

---
+++ src/feature/ide/utils/debounce.ts
@@ -0,0 +1,21 @@
+/**
+ * Debounce function.
+ *
+ * @param {Function} callback - The function to debounce.
+ * @param {number} timeout - The debounce timeout in milliseconds.
+ */
+const debounce = <T extends (...args: any[]) => any>(
+  callback: T,
+  timeout: number = 300,
+): ((...args: Parameters<T>) => void) => {
+  let timer: NodeJS.Timeout;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, timeout);
+  };
+};
+
+export default debounce;
