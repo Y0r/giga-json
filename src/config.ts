a038a109c2db88a823c682c6c0b233e219f5343a
Y0r <panchyk.yuriu@gmail.com>
2026-03-05 21:57:02 -0500

    chore(config): Introduce a new configuration property for managing debug of file creation process

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ export const config = {
   appName: import.meta.env.VITE_APP_NAME ?? "giga_json",
   dev: import.meta.env.VITE_DEV ?? true,
   debugMode: import.meta.env.VITE_DEBUG_MODE ?? false,
+  debugFileCreation: import.meta.env.VITE_DEBUG_FILE_CREATION ?? false,
   debugDisplayErrors: import.meta.env.VITE_DISPLAY_ERRORS ?? false,
 };
