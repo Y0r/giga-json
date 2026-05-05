5bf926d644e30c660533a3f043aa46b854f0937f
Y0r <panchyk.yuriu@gmail.com>
2026-05-04 22:25:10 -0400

    fix(menu): Refactors ide menus to resolve coding standards issue

---
+++ src/feature/ide/components/Menu/MainMenu.schema.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import {
+  VscCopy,
+  VscFile,
+  VscFolder,
+  VscInsert,
+  VscSaveAs,
+  VscTrash,
+} from "react-icons/vsc";
+
+import { useFiles } from "@/feature/ide/hooks/useFiles";
+import { useMenuSchema as useTabsMenuSchema } from "@/feature/ide/components/Menu/TabsMenu.schema";
+
+/**
+ * Menu schema for the main menu.
+ */
+export const useMenuSchema = () => {
+  const { createEmptyFile, createFromFile, saveFileAs } = useFiles();
+  const tabsMenuSchema = useTabsMenuSchema();
+
+  return {
+    orientation: "horizontal" as const,
+    items: [
+      {
+        label: "File",
+        icon: null,
+        items: [
+          {
+            label: "New File",
+            icon: <VscFile />,
+            action: {
+              type: "callback" as const,
+              onClick: createEmptyFile,
+            },
+          },
+          {
+            label: "Open",
+            icon: <VscFolder />,
+            action: {
+              type: "callback" as const,
+              onClick: createFromFile,
+            },
+          },
+          { type: "separator" as const },
+          {
+            label: "Save as...",
+            icon: <VscSaveAs />,
+            action: {
+              type: "callback" as const,
+              onClick: saveFileAs,
+            },
+          },
+          { type: "separator" as const },
+          {
+            label: "Clear storage",
+            icon: <VscTrash />,
+            action: {
+              type: "callback" as const,
+              onClick: () => {
+                // @todo register event with notification.
+                // @todo remove specific items from storage.
+                // @todo inform user about success.
+                window.localStorage.clear();
+                location.reload();
+              },
+            },
+          },
+        ],
+      },
+      {
+        label: "Edit",
+        icon: null,
+        items: [
+          {
+            label: "Copy",
+            icon: <VscCopy />,
+            shortcut: "Ctrl+C",
+            action: {
+              type: "callback" as const,
+              onClick: () => console.log("Copy"),
+            },
+          },
+          {
+            label: "Paste",
+            icon: <VscInsert />,
+            action: {
+              type: "callback" as const,
+              onClick: () => console.log("Paste"),
+            },
+          },
+        ],
+      },
+      {
+        label: "Window",
+        icon: null,
+        items: [
+          {
+            label: "Editor Tabs",
+            icon: null,
+            items: [...tabsMenuSchema.items],
+          },
+        ],
+      },
+    ],
+  };
+};
---
+++ src/feature/ide/components/Menu/TabsMenu.schema.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { VscGithub } from "react-icons/vsc";
+import { useTabs } from "@/feature/ide/hooks/useTabs";
+import { EditorFile } from "@/feature/ide/state/ide.types";
+
+interface TabsMenuSchemaProps {
+  focusedTabId?: EditorFile["id"];
+}
+
+/**
+ * Menu schema for the tab's menu.
+ */
+export const useMenuSchema = (
+  focusedTabId?: TabsMenuSchemaProps["focusedTabId"],
+) => {
+  const {
+    closeCurrentTab,
+    closeOtherTabs,
+    closeAllTabs,
+    closeTabsToTheLeft,
+    closeTabsToTheRight,
+    reopenClosedTab,
+  } = useTabs(focusedTabId);
+
+  return {
+    orientation: "vertical" as const,
+    items: [
+      {
+        label: "Close",
+        icon: null,
+        action: {
+          type: "callback" as const,
+          onClick: closeCurrentTab,
+        },
+      },
+      {
+        label: "Close Other Tabs",
+        icon: null,
+        action: {
+          type: "callback" as const,
+          onClick: closeOtherTabs,
+        },
+      },
+      {
+        label: "Close All Tabs",
+        icon: null,
+        action: {
+          type: "callback" as const,
+          onClick: closeAllTabs,
+        },
+      },
+      {
+        label: "Close Tabs to the Left",
+        icon: null,
+        action: {
+          type: "callback" as const,
+          onClick: closeTabsToTheLeft,
+        },
+      },
+      {
+        label: "Close Tabs to the Right",
+        icon: null,
+        action: {
+          type: "callback" as const,
+          onClick: closeTabsToTheRight,
+        },
+      },
+      { type: "separator" as const },
+      {
+        label: "Reopen Closed Tab",
+        icon: null,
+        action: {
+          type: "callback" as const,
+          onClick: reopenClosedTab,
+        },
+      },
+      { type: "separator" as const },
+      {
+        label: "Rename file...",
+        icon: null,
+        action: {
+          type: "callback" as const,
+          onClick: () => console.log("rename file"),
+        },
+      },
+      {
+        label: "Create Gist...",
+        icon: <VscGithub />,
+        action: {
+          type: "callback" as const,
+          onClick: () => console.log("create gist"),
+        },
+      },
+    ],
+  };
+};
