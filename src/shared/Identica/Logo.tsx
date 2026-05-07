86fa7eb344b309e3cad680a4e50a0e9f57338e2a
Y0r <panchyk.yuriu@gmail.com>
2026-05-06 20:49:59 -0400

    fix(app): Resolves minor issue with logo asset path.

--- src/shared/Identica/Logo.tsx
+++ src/shared/Identica/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
   return (
     <Flex gap={1}>
       <Icon size={"md"} color={"white"}>
-        <Image src="/assets/logo-light.svg" alt="Sycamore Tree" />
+        <Image src="./assets/logo-light.svg" alt="Sycamore Tree" />
       </Icon>
 
       <Text fontSize={"sm"} fontWeight={"bold"} color={"white"}>
